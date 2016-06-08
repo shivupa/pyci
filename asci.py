f186b66163fc54fc59ac81dbfba099899aa19d42
Shiv <upadhya1@duq.edu>
2016-06-08 08:47:01 -0500

    Some stuff and fixed that I don't understand and won't work

--- asci.py
+++ asci.py
@@ -4,11 +4,12 @@ import scipy.linalg as spla
 import numpy as np
 from functools import reduce
 import pyscf
+import itertools
+import h5py
 from pyscf import gto, scf, ao2mo, fci
 '''
 Energy curve by FCI
 '''
-
 #############
 # INPUT
 #############
@@ -27,11 +28,13 @@ tdets = 500
 #############
 # INITIALIZE
 #############
-size_basis = len(s[0])
-C_new = np.zero(sp.special.binom(size_basis,mol.nelectron))
-C_old = np.zero(sp.special.binom(size_basis,mol.nelectron))
+s = mol.intor('cint1e_nuc_sph')
+size_basis = len(s[0])*2
+print size_basis
+C_new = np.zeros(sp.special.binom(size_basis,mol.nelectron))
+C_old = np.zeros(sp.special.binom(size_basis,mol.nelectron))
 C_old[0] = 1
-C_new = np.zero(sp.special.binom(size_basis,mol.nelectron))
+C_new = np.zeros(sp.special.binom(size_basis,mol.nelectron))
 
 myhf = scf.RHF(mol)
 E = myhf.kernel()
@@ -40,42 +43,31 @@ core = np.argsort(C_old)
 detlist = np.array(list(itertools.combinations(np.arange(size_basis),mol.nelectron)))
 
 H = np.zeros((len(C_old),len(C_old)))
+print "A",np.size(H)
 
 #############
 # LOOP
 #############
 # get fock matrix
-h1e = mf.get_hcore(mol)
-s1e = mf.get_ovlp(mol)
+h1e = myhf.get_hcore(mol)
+s1e = myhf.get_ovlp(mol)
 ijkl = ao2mo.incore.full(pyscf.scf._vhf.int2e_sph(mol._atm, mol._bas, mol._env), myhf.mo_coeff, compact=False)
+mocc = myhf.mo_coeff[:,myhf.mo_occ>0]
+mvir = myhf.mo_coeff[:,myhf.mo_occ==0]
+ao2mo.general(mol, (mocc,mocc,mocc,mocc), 'tmp.h5', compact=False)
+feri = h5py.File('tmp.h5')
+gpqpq = np.array(feri['eri_mo'])
+print np.shape(gpqpq)
 # eq 5
-for i in len(H):
-    occ = det[i]
-    virt = np.set1d(np.arange(size_basis),occ)
+for i in range(len(H)):
+    occ = detlist[i]
+    virt = np.setdiff1d(np.arange(size_basis),occ)
     for p in occ:
         H[i,i] += h1e[p,p]
         for q in occ:
             if p != q:
-                if(i>j)
-                    ij = i*(i+1)/2 + j;
-                else
-                    ij = j*(j+1)/2 + i;
-                h+=0.5*(ijkl[ij,ij]
-
-for i in core:
-    occ = det[i]
-    virt = np.set1d(np.arange(size_basis),occ)
-    for p in occ:
-        H[i,i] =
-        for r in occ:
-
-
-
-for i in core:
-    occ = det[i]
-    virt = np.set1d(np.arange(size_basis),occ)
-    for p in occ:
-        for r in virt:
+                
+                print H[i,i]
 
 
 
