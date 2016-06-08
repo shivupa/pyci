547e4acf27f8eab68f7ce86290fe780c1b9d0572
Shiv <upadhya1@duq.edu>
2016-06-07 22:38:07 -0500

    Some stuff and fixed that I don't understand and won't work

--- testmp2.py
+++ testmp2.py
@@ -4,19 +4,17 @@ import scipy.linalg as spla
 import numpy as np
 from functools import reduce
 import pyscf
-from pyscf import gto, scf, ao2mo, fci, mp
+from pyscf import gto, scf, ao2mo, fci, mp, ao2mo
 def myump2(mf):
-    import numpy
-    from pyscf import ao2mo
     # As UHF objects, mo_energy, mo_occ, mo_coeff are two-item lists
     # (the first item for alpha spin, the second for beta spin).
     mo_energy = mf.mo_energy
     mo_occ = mf.mo_occ
     mo_coeff = mf.mo_coeff
-    o = numpy.hstack((mo_coeff[0][:,mo_occ[0]>0] ,mo_coeff[1][:,mo_occ[1]>0]))
-    v = numpy.hstack((mo_coeff[0][:,mo_occ[0]==0],mo_coeff[1][:,mo_occ[1]==0]))
-    eo = numpy.hstack((mo_energy[0][mo_occ[0]>0] ,mo_energy[1][mo_occ[1]>0]))
-    ev = numpy.hstack((mo_energy[0][mo_occ[0]==0],mo_energy[1][mo_occ[1]==0]))
+    o = np.hstack((mo_coeff[0][:,mo_occ[0]>0] ,mo_coeff[1][:,mo_occ[1]>0]))
+    v = np.hstack((mo_coeff[0][:,mo_occ[0]==0],mo_coeff[1][:,mo_occ[1]==0]))
+    eo = np.hstack((mo_energy[0][mo_occ[0]>0] ,mo_energy[1][mo_occ[1]>0]))
+    ev = np.hstack((mo_energy[0][mo_occ[0]==0],mo_energy[1][mo_occ[1]==0]))
     no = o.shape[1]
     nv = v.shape[1]
     noa = sum(mo_occ[0]>0)
@@ -26,7 +24,7 @@ def myump2(mf):
     g = eri - eri.transpose(0,3,2,1)
     eov = eo.reshape(-1,1) - ev.reshape(-1)
     de = 1/(eov.reshape(-1,1) + eov.reshape(-1)).reshape(g.shape)
-    emp2 = .25 * numpy.einsum('iajb,iajb,iajb->', g, g, de)
+    emp2 = .25 * np.einsum('iajb,iajb,iajb->', g, g, de)
     return emp2
 mol = gto.M(
     atom = [['O', (0.000000000000,  -0.143225816552,   0.000000000000)],
@@ -40,5 +38,4 @@ myhf = scf.RHF(mol)
 E = myhf.kernel()
 ijkl = ao2mo.incore.full(pyscf.scf._vhf.int2e_sph(mol._atm, mol._bas, mol._env), myhf.mo_coeff, compact=False)
 print E
-mp.MP2(mol)
-print mp.kernel()[0]
+print myump2(myhf)
