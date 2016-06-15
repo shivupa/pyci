56028ede63a1f5fb1b91309be9638de049bd7496
Shiv <upadhya1@duq.edu>
2016-06-14 23:28:09 -0400

    Some stuff and fixed that I don't understand and won't work

--- asci.py
+++ asci.py
@@ -18,8 +18,8 @@ mol = gto.M(
     verbose = 1,
     unit='b'
 )
-cdets = 250
-tdets = 500
+cdets = 25
+tdets = 50
 #############
 # FUNCTIONS
 #############
@@ -35,5 +35,40 @@ def create_PYSCF_fcidump():
 #############
 # INITIALIZE
 #############
-#try except to open file
-create_PYSCF_fcidump()
+myhf = scf.RHF(mol)
+E = myhf.kernel()
+c = myhf.mo_coeff
+h1e = reduce(np.dot, (c.T, myhf.get_hcore(), c))
+eri = ao2mo.kernel(mol, c)
+print h1e
+print eri
+print np.shape(h1e),np.shape(eri)
+print mol.nelectron, np.shape(h1e)[0]*2
+num_occ = mol.nelectron
+num_virt = ((np.shape(h1e)[0]*2)-mol.nelectron)
+bitstring = "1"*num_occ
+bitstring += "0"*num_virt
+print bitstring
+starting_amplitude =0.1
+detdict = {bitstring:starting_amplitude}
+print detdict
+# a^dagger_i a_j |psi>
+
+for det in detdict:
+    occ_index = []
+    virt_index = []
+    count = 0
+    for i in det:
+        if i == "1":
+            occ_index.append(count)
+        else:
+            virt_index.append(count)
+        count +=1
+    print occ_index
+    print virt_index
+    for i in occ_index:
+        for j in virt_index:
+            temp_det = list(det)
+            temp_det[i] = "0"
+            temp_det[j] = "1"
+            print ''.join(temp_det)
