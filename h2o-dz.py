42e9b90263d1ceffe75e6de642cc85b852545da3
Shiv <upadhya1@duq.edu>
2016-07-07 23:52:06 -0400

    timing test. notes

--- h2o-dz.py
+++ h2o-dz.py
@@ -12,6 +12,7 @@ import pyscf.tools as pt
 import copy
 import matplotlib.pyplot as plt
 from utils import *
+import time
 #############
 # INPUT
 #############
@@ -28,5 +29,25 @@ mol = gto.M(
     unit='b',
     symmetry=True
 )
-
+start = time.time()
 asci(mol,50,100,10e-9,iter_min=10)
+end = time.time()
+print("RUN TIME ASCI: (h:m:s)")
+m, s = divmod(end-start, 60)
+h, m = divmod(m, 60)
+print("%d:%02d:%02d" % (h, m, s))
+"""
+print("FCI from PYSCF")
+start = time.time()
+myhf = scf.RHF(mol)
+E = myhf.kernel()
+c = myhf.mo_coeff
+cisolver = fci.FCI(mol, c)
+efci = cisolver.kernel(nroots=4)[0] + mol.energy_nuc()
+print(efci)
+end = time.time()
+print("RUN TIME PYSCF: (h:m:s)")
+m, s = divmod(end-start, 60)
+h, m = divmod(m, 60)
+print("%d:%02d:%02d" % (h, m, s))
+"""
